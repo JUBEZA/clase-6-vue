be7f7857ea47b0ed5a923fad05ac5f6de3d94529
Juan Bezanilla <juanbezanillaz@gmail.com>
2023-06-12 18:04:56 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app-component">
+    <EjemploWatch />
+    <MiComponente />
+    <MiComponente firstName="Félix" />
+  </div>
+</template>
+
+<script>
+import EjemploWatch from './components/EjemploWatch.vue';
+import MiComponente from './components/MiComponente.vue';
+
+export default {
+  name: 'App',
+  components: {
+    EjemploWatch,
+    MiComponente,
+  },
+};
+</script>
+
+<style>
+#app-component {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MiComponente.vue
@@ -0,0 +1,45 @@
+<template>
+  <p class="card">Hola {{ prefijo }} {{ firstName }}</p>
+  <button @click="nuevoAlumno">Añadir a Esteban</button>
+  <ul>
+    <li v-for="(alumno, i) in alumnos" :key="i">
+      <UnAlumno :name="alumno">
+    </li>
+  </ul>
+</template>
+
+<script>
+import UnAlumno from './UnAlumno.vue';
+
+export default {
+  name: 'MiComponente',
+  components: {
+    UnAlumno
+  },
+  props: {
+    firstName: {
+      type: String,
+      default: 'Santiago',
+    },
+    lastName: String,
+  },
+  data() {
+    return {
+      prefijo: 'Mr.',
+      alumnos: ['Santiago', 'Andrés', 'Fidel', 'Carolina'],
+    };
+  },
+  methods: {
+    nuevoAlumno() {
+      this.alumnos.push("Esteban")
+    }
+  }
+};
+</script>
+
+<style scoped>
+.card {
+  background: blue;
+  color: white;
+}
+</style>
